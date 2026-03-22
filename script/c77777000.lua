1f3a9cbd365333ab49ff0207671a6ba646e10fe1
Rene Lino Herrera <70409502+FunnyBones777@users.noreply.github.com>
2026-03-22 12:37:53 -0400

    Another small fix...

--- script/c77777000.lua
+++ script/c77777000.lua
@@ -2,6 +2,7 @@
 --Scripted by FunnyBones777
 local s,id=GetID()
 function s.initial_effect(c)
+	Pendulum.AddProcedure(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
