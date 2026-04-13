b6cfddaa5a63a75b6658ac18e43707349d66fec9
Rene Lino Herrera <70409502+FunnyBones777@users.noreply.github.com>
2026-04-13 12:31:14 -0400

    A new Shadow Sunflower!

---
+++ script/c77777014.lua
@@ -0,0 +1,38 @@
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMixN(c,true,true,s.ffilter,2)
+	--Excavate the top card of your Deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_DECKDES)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.ffilter(c,fc,sumtype,tp)
+	return c:IsAttribute(ATTRIBUTE_DARK)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,1) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.IsPlayerCanDiscardDeck(tp,1) then return end
+	Duel.ConfirmDecktop(tp,1)
+	local g=Duel.GetDecktopGroup(tp,1)
+	local tc=g:GetFirst()
+	if tc:IsAttribute(ATTRIBUTE_DARK) then
+		Duel.DisableShuffleCheck()
+		Duel.SendtoGrave(tc,REASON_EFFECT|REASON_EXCAVATE)
+		local ccount=Duel.GetOperatedGroup():FilterCount(Card.IsLocation,nil,LOCATION_GRAVE)
+		if ccount>0 then
+			Duel.Draw(tp,1,REASON_EFFECT)
+		end
+	else
+		Duel.MoveToDeckBottom(tc)
+	end
+end
